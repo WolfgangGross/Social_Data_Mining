8e95e97dec206f5d428f34d1a844ded4c2f4c638
Wolfgang Gross <wolfigross@gmx.de>
2014-10-22 15:43:29 +0200

    rework on statistics
    reworked count Timestamps
    add statistics minute / hour

--- load_data.r
+++ load_data.r
@@ -4,13 +4,16 @@
 #setting envioronment
 library(ggplot2)
 library(data.table)
+library(bit64)
 setwd("~/Documents/programmierung/Social Data Mining All")
 
 
 #####################
 #loading data in
 dat <- read.csv('13July_19xxxx.csv',sep='\t')
-#dat <- fread('13July_19xxxx.csv',sep='\t')
+dat <- read.csv('13July_19xxxx_with_semi2.csv',sep=';')
+#faster
+dat <- fread('13July_19xxxx_with_semi2.csv',sep=';')
 
 #save data in file to load it fster
 saveRDS(dat, file="data.Rda")
--- manipulate.r
+++ manipulate.r
@@ -3,13 +3,19 @@
 count = c()
 #remove the doubles form dat$Timestamp; get unique
 times <- unique(dat$Timestamp)
-for (i in times ){count <- append(count,sum(dat$Timestamp==i))}
+#tabel gets number of elements
+count  <- data.frame(table(dat$Timestamp))
+
+#sum up per minute and per hour
+sumMinute <- c()
+for(i in 1:as.integer(length(count$Freq)/60)){sumMinute <- append(sumMinute,sum(count$Freq[(i*60-59):(i*60)]))}
+sumHour <- c()
+for(k in 1:(length(sumMinute)/60)){sumHour <- append(sumHour,sum(sumMinute[(k*60-59):(k*60)]))}
 
 #####################
 #plot
-#get times no as factors
 #convert Time into real time format
 timeReal <- strptime(as.character(times),format="%Y%m%d%H%M%S")
 
-df <- data.frame(timeReal,count)
+df <- data.frame(timeReal,count$Freq)
 ggplot(df,aes(x=timeReal,y=count)) + geom_line()
